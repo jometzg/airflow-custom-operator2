114b97eab968db70bdadd4aea6a949d5ac3603e3
John Metzger <john.metzger@microsoft.com>
2025-04-14 15:48:52 +0100

    update package

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='airflow-custom-operator',
-    version='0.1.0',
+    version='0.2.0',
     author='John Metzger',
     author_email='john.metzger@lseg.com',
     description='A custom Apache Airflow operator',
